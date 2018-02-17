adda1025104d641fa327ac5afe6f006eafd1a628
Gary <gary.mayfield@hotmail.co.uk>
2018-02-17 11:44:27 +0000

    Separate logical units

---
+++ enemy.py
@@ -0,0 +1,67 @@
+import random
+
+ENEMY_NAMES = (
+		"Minotaur",
+		"Hydra",
+		"Griffin",
+		)
+
+class Enemy:
+	name=None
+	hp=100
+	maxHp = 100
+	stat_defense=0
+	stat_attack=0
+	level=1
+	isPlayer=False
+	weapon=None
+
+	def __init__(self,level,weapon):
+		self.name = random.choice(ENEMY_NAMES)
+		self.weapon = weapon
+		self.level = level
+		self.fixStats()
+		self.hp = self.maxHp
+	
+	def fixStats(self):
+		"""fixes the stats (attack/defense) after the level
+		has been changed"""
+		level = self.level
+		if self.isPlayer:
+			level += 1 #slight advantage for the player
+		self.stat_attack = 40(level*5)
+		self.stat_defense = 30(level*5)
+		self.maxHp  = 100(10*(level-1))
+	
+	def hurt(self, amount):
+		"""hurts the object, removes damaged based on the object's
+		defense rating"""
+		amount -= (self.stat_defence/2)
+		self.hp -= amount
+		return self.hp
+	
+	def attack(self, target):
+		"""attacks a target, and removes damaged based on the object
+		attack rating"""
+		damage = ((self.weapon.attack+self.stat_attack)/2)+random.choice(range(self.stat_attack/6, self.stat_attack/4))
+		target.hurt(damage)
+		return damage
+	
+	def heal(self, value):
+		"""heals the current object"""
+		oldHP = self.hp
+		self.hp += value
+		if self.hp > self.maxHp:
+			self.hp = self.maxHp
+		return self.hp - oldHp
+	
+	def think(self, target):
+		"""AI for the enemy"""
+		if self.hp <0: return # we can't think if we're dead
+		if self.hp/self.maxHp < 0.4 and random.choice(True, False, False)):
+			amount = self.heal(self.level*10)
+			print("Enemy used a healing potion, recovered %s HP!") % amount
+		else:
+			damage=self.attack(target)
+			print("Enemy %s attacks, %s loses %s HP") % (self.name, target.name, damage)
+
---
+++ player.py
@@ -0,0 +1,37 @@
+from enemy import Enemy
+from weapons import Dagger
+
+class Player(Enemy):
+	experience = 0
+	inventory = []
+	isPlayer = True
+
+	def __init__(self):
+		self.weapon = Dagger()
+		self.name = input("What is your name?")
+		self.level = 2
+		self.fixStats()
+		self.hp = self.maxHp
+	
+	def giveExp(self, amount):
+		
+		print("You have gained %s experience points") % amount
+		self.experience += amount
+
+		if. self.experience > (self.level+1)^2:
+			self.level += 1
+			self.fixStats()
+			print("You have gained a level!")
+			print("Level - %s") % self.level
+			print("Attack - %s") % self.stat_attack
+			print("Defense - %s") % self.stat_defense
+			print("Max HP - %s") % self.maxHp
+		else:
+			print("%s experience points until next level") % ((self.level+1)^2)-self.experience
+	
+	def giveItem(self, item):
+		"""give the player an item, add to weapons or inventory slot"""
+		if item.isWeapon:
+			self.weapon = item
+		else:
+		self.inventory.append(item)
